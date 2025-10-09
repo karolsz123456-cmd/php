8803266046f9fa4937241477df4285ffef1730bb
karolsz123456-cmd <karolsz123456@gmail.com>
2025-10-09 12:24:01 +0200

    09.10.2025

---
+++ Wyrażenia regularne/Wyszukiwanie_wyrazu_w_tekście.php
@@ -0,0 +1,13 @@
+<?php
+$wzorzec = '/byc/';
+$tekst = 'byc albo nie byc';
+if (preg_match($wzorzec, $tekst))
+echo 'znaleziono wzorzec';
+else
+echo 'nie znaleziono nic';
+
+
+echo 'znaleziono:'.preg_match_all($wzorzec, $tekst);
+?>
+
+
