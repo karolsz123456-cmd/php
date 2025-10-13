cee0e79b37c56a40ef88540d33834bff6a31ec2b
karolsz123456-cmd <karolsz123456@gmail.com>
2025-10-13 12:12:24 +0200

    13.10.2025

---
+++ Wyrażenia regularne/wyszukiwanie_daty.php
@@ -0,0 +1,8 @@
+<?php //wyszkuwanie daty
+$wzorzec = 
+'/([0123])?\d)+[-\.\/]+([01]?\d)+[-\.\/]+(\d{4})/';
+$tekst(preg_match($wzorzec,$tekst))
+echo "znaleziono date";
+else
+    echo"nie znaleziono daty"
+?>
